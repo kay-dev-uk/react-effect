2e8496fd87068b261914bd203ea195e6ed17df42
kay-dev-uk <kay-dev-uk@gmail.com>
2023-02-21 17:29:40 +0000

    minor fixes

--- superhero/src/App.jsx
+++ superhero/src/App.jsx
@@ -1,16 +1,11 @@
-import React, {useState ,useEffect} from 'react';
-import { FactBox, TodoForm, TodoList } from './components';
+import React from 'react';
+import FetchSuperhero from './components';
 
 function App() {
-//   const [inputText, setInputText] = useState('');
-//   const [todos, setTodos] = useState([]);
 
   return (
     <div>
-      <header>Superhero</header>
-      {/* <TodoForm inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
-      <FactBox todos={todos}/> */}
+      <FetchSuperhero />
     </div>
   );
 }
